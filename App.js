618c563438d9b584045629475078002ab3e6651b
akinGC <akarshgc1998@gmail.com>
2023-04-28 15:31:53 +0530

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ahhhhh into a developer journey you dont have any one to wish you so let me wish myself all the best akins lord aka akarsh</Text>
+      <Text>AkinsLord one day!</Text>
       <StatusBar style="auto" />
     </View>
   );
